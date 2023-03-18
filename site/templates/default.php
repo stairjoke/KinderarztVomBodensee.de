b27a5835a065ea676a55814751fa0dd34eb9b091
Wenzel Massag <mail@wmassag.de>
2023-03-18 09:18:59 +0000

    starting template development

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,18 @@
-<h1><?= $page->title() ?></h1>
+<!DOCTYPE html>
+<html lang="de">
+	<head>
+		<meta charset="UTF-8">
+		
+		<title><?= $site->title() ?> — <?= $page->title() ?></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<meta name="description" content="<?= ($page->description()->exists()) ? $page->description() : $site->description() ?>">
+		
+		<link href="assets/foundation.css" rel="stylesheet">
+	</head>
+	<body>
+		<header></header>
+		<main><?= $page->inhalt()->toBlocks() ?></main>
+		<footer></footer>
+	</body>
+</html>
