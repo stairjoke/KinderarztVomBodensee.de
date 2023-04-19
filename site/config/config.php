e9c256ef2da15f913ff6e7d447dedf72a8e45c76
Wenzel Massag <mail@wmassag.de>
2023-04-19 15:46:19 +0000

    added default blocks config

--- site/config/config.php
+++ site/config/config.php
@@ -1,5 +1,31 @@
 <?php
 return [
-	'debug' => true
+	'debug' => true,
+	'blocks' => [
+		'fieldsets' => [
+			'text' => [
+				'label' => 'Text',
+				'type' => 'group',
+				'fieldsets' => [
+					'text', 'heading', 'list'
+				]
+			],
+			'media' => [
+				'label' => 'Medien',
+				'type' => 'group',
+				'fieldsets' => [
+					'image', 'self-hosted-video'
+				]
+			],
+			'code' => [
+				'label' => 'Code',
+				'type' => 'group',
+				'open' => false,
+				'fieldsets' => [
+					'markdown', 'code'
+				]
+			]
+		]
+	]
 ];
 ?>
