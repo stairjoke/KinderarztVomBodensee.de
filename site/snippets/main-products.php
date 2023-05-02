7a80835eeab1e4d20f442a032a0a6986f9b28f51
Wenzel Massag <mail@wmassag.de>
2023-05-02 18:29:13 +0000

    products page layout for all viewport sizes

--- site/snippets/main-products.php
+++ site/snippets/main-products.php
@@ -1,12 +1,11 @@
 <main>
 	<?= $page->blocks()->toBlocks() ?>
 	<?php foreach($page->children()->listed() as $product) : ?>
-		<article class="hasSidecar product">
-			<aside>
-				<?= $productImage = $product->files()->first() ?>
-			</aside>
+		<article class="hasSidecar productCard">
+			<h2 class="mobile"><a href="<?= $product->url() ?>"><?= $product->title() ?></a></h2>
+			<aside><?= $productImage = $product->files()->first() ?></aside>
 			<main>
-				<h2><a href="<?= $product->url() ?>"><?= $product->title() ?></a></h2>
+				<h2 class="desktop"><a href="<?= $product->url() ?>"><?= $product->title() ?></a></h2>
 				<?= $product->summary()->kirbytext() ?>
 			</main>
 		</article>
