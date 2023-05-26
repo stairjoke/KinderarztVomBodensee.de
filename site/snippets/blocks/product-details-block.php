7d824291333b4cf6cd6a9d4109d4db405a81a127
Wenzel Massag <mail@wmassag.de>
2023-05-26 15:49:23 +0200

    product details table markup

--- site/snippets/blocks/product-details-block.php
+++ site/snippets/blocks/product-details-block.php
@@ -1,3 +1,17 @@
 <?php
 	// Product Details table Block — Der Kinderarzt vom Bodensee
 ?>
+
+<table class="productDetails">
+	<thead>
+		<tr>
+			<th scope="col"><?= $block->cell1()->kirbytextinline() ?></th>
+			<th scope="col"><?= $block->cell2()->kirbytextinline() ?></th>
+			<th scope="col"><?= $block->cell3()->kirbytextinline() ?></th>
+			<th scope="col"><?= $block->cell4()->kirbytextinline() ?></th>
+		</tr>
+	</thead>
+	<tbody>
+		<?= $block->rows()->toBlocks() ?>
+	</tbody>
+</table>
--- site/snippets/blocks/product-details-row.php
+++ site/snippets/blocks/product-details-row.php
@@ -1,3 +1,10 @@
 <?php
 	// Product Details table Block -> row — Der Kinderarzt vom Bodensee
 ?>
+
+<tr>
+	<td><?= $block->cell1()->kirbytextinline() ?></td>
+	<td><?= $block->cell2()->kirbytextinline() ?></td>
+	<td><?= $block->cell3()->kirbytextinline() ?></td>
+	<td><?= $block->cell4()->kirbytextinline() ?></td>
+</tr>
