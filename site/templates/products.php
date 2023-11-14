ae43d809b9b7d3bb22bf129dc49ac6820bb90db2
Wenzel Massag <mail@wmassag.de>
2023-11-14 12:14:42 +0100

    solves #11

--- site/templates/products.php
+++ site/templates/products.php
@@ -1,9 +1,10 @@
+<?php snippet('head'); ?>
 <main>
 	<?= $page->blocks()->toBlocks() ?>
 	<?php foreach($page->children()->listed() as $product) : ?>
 		<article class="hasSidecar productCard">
 			<h2 class="mobile"><a href="<?= $product->url() ?>"><?= $product->title() ?></a></h2>
-			<aside><?= $productImage = $product->files()->first() ?></aside>
+			<aside><?= $product->hero()->toFile() ?></aside>
 			<main>
 				<h2 class="desktop"><a href="<?= $product->url() ?>"><?= $product->title() ?></a></h2>
 				<?= $product->summary()->kirbytext() ?>
@@ -11,3 +12,4 @@
 		</article>
 	<?php endforeach ?>
 </main>
+<?php snippet('foot'); ?>
