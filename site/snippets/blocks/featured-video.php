2a73f81fe0252efcd746b4b2ca0c8874479f2138
Wenzel Massag <mail@wmassag.de>
2023-11-14 13:31:29 +0100

    updated featured video block, added css

--- site/snippets/blocks/featured-video.php
+++ site/snippets/blocks/featured-video.php
@@ -1,18 +1,18 @@
 <?php
   // Featured Videos Block — Der Kinderarzt vom Bodensee
 ?>
-
+<section class="featured-videos-block">
 <?php
   $featuredVideos = $block->featured()->toPages();
   foreach($featuredVideos as $video) : ?>
     <div class="video">
-      <?= ($video->hasImages()) ? $video->image() : '<img />' ?>
+      <?= ($video->hasImages()) ? $video->image() : '' ?>
       <h2><a href="<?= $video->url() ?>" target="_blank">
         <svg class="icon inline" aria-label="YouTube Logo">
           <use href="/assets/images/iconSprite.svg#youtube"></use>
-        </svg>
-        <span><?= $video->title() ?></span>
+        </svg>&nbsp;<span><?= $video->title() ?></span>
       </a></h2>
       <?= $video->beschreibung()->kt() ?>
     </div>
   <?php endforeach; // $featuredVideos ?>
+  </section>
